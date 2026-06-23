dd5701d3a1a6cf60db7bbf02f66697f83975c96a
HongNhienTran <hongnhientran.contact@gmail.com>
2026-06-23 08:21:22 +0700

    first commit

---
+++ frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,50 @@
+import {create} from "zustand";
+import {toast} from "sonner";
+import { authService } from "@/services/authService";
+import type { authState } from "@/types/store";
+
+export const useAuthStore = create<authState>((set, get)=> ({
+    accessToken: null,
+    user: null,
+    loading: false,
+
+    clearState: () => {
+        set({ accessToken: null, user: null });
+    },
+
+    signUp: async (username, password, email, firstName, lastName) => {
+        try {
+            set({loading: true});
+            await authService.signUp(username, password, email, firstName, lastName)
+            toast.success("Signed Up Successfully!");
+        } catch (error) {
+            console.error("Sign up failed:", error);
+            toast.error("Sign up failed. Please try again.");
+        }finally{
+            set({loading: false})
+        }
+    },
+    signIn: async(username, password) =>{
+        try {
+            set({loading: true});
+            const { accessToken} = await authService.signIn(username, password);
+            set({accessToken});
+            toast.success("Signed In Successfully!");
+        } catch (error) {
+            console.error("Sign in failed:", error);
+            toast.error("Sign in failed. Please try again.");
+        }finally{
+            set({loading: false})
+        }
+    },
+    signOut: async () => {
+        try {
+            get().clearState();
+            await authService.signOut();
+            toast.success("Signed Out Successfully!");
+        } catch (error) {
+            console.error("Sign out failed:", error);
+            toast.error("Sign out failed. Please try again.");
+        }
+    }
+}))
---
+++ frontend/src/types/store.ts
@@ -0,0 +1,11 @@
+import { User } from "./user";
+
+
+export interface authState {
+  accessToken: string | null;
+  user: User | null;
+  loading: boolean;
+
+  signUp: (username: string, password: string, email: string, firstName: string, lastName: string) => Promise<void>;
+  signIn: (username: string, password: string) => Promise<void>;
+}
